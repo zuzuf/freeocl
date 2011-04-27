23d84dcb6552e06e7bc310ad4694ad0ee1e4a34f
zuzuf <zuzuf@ebb5cd76-4019-73bf-d3cb-c11e5ea2855e>
2011-04-27 11:17:03 +0000

    * added AUTHORS, TODO, COPYING, README * new struct command_common is a common component of all command_* structs (contains event handles required by the scheduler)

--- src/commandqueue.h
+++ src/commandqueue.h
@@ -8,35 +8,39 @@
 
 namespace FreeOCL
 {
-	struct command_read_buffer
+	struct command_common
 	{
 		cl_command_type type;
+		cl_event		event;
+		cl_uint			num_events_in_wait_list;
+		const cl_event	*event_wait_list;
 	};
 
-	struct command_write_buffer
+	struct command_read_buffer : public command_common
 	{
-		cl_command_type type;
 	};
 
-	struct command_copy_buffer
+	struct command_write_buffer : public command_common
 	{
-		cl_command_type type;
 	};
 
-	struct command_map_buffer
+	struct command_copy_buffer : public command_common
 	{
-		cl_command_type type;
 	};
 
-	struct command_unmap_buffer
+	struct command_map_buffer : public command_common
 	{
-		cl_command_type type;
+	};
+
+	struct command_unmap_buffer : public command_common
+	{
 	};
 
 	union command
 	{
-		cl_command_type	type;
-		command_read_buffer	read_buffer;
+		cl_command_type			type;
+		command_common			common;
+		command_read_buffer		read_buffer;
 		command_write_buffer	write_buffer;
 	};
 }
--- src/mem.cpp
+++ src/mem.cpp
@@ -146,6 +146,32 @@ extern "C"
 
 		return CL_SUCCESS;
 	}
+
+	cl_int clEnqueueReadBuffer (cl_command_queue command_queue,
+								cl_mem buffer,
+								cl_bool blocking_read,
+								size_t offset,
+								size_t cb,
+								void *ptr,
+								cl_uint num_events_in_wait_list,
+								const cl_event *event_wait_list,
+								cl_event *event)
+	{
+
+	}
+
+	cl_int clEnqueueWriteBuffer (cl_command_queue command_queue,
+								 cl_mem buffer,
+								 cl_bool blocking_write,
+								 size_t offset,
+								 size_t cb,
+								 const void *ptr,
+								 cl_uint num_events_in_wait_list,
+								 const cl_event *event_wait_list,
+								 cl_event *event)
+	{
+
+	}
 }
 
 _cl_mem::~_cl_mem()
