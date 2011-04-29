73293581e7f9de0bd941fde6366fa16695a00b9a
zuzuf <zuzuf@ebb5cd76-4019-73bf-d3cb-c11e5ea2855e>
2011-04-29 17:54:45 +0000

    * implemented several integer functions

--- include/FreeOCL/integer.h
+++ include/FreeOCL/integer.h
@@ -18,4 +18,65 @@
 #ifndef __FREEOCL_OPENCL_C_PREINCLUDE_INTEGER_H__
 #define __FREEOCL_OPENCL_C_PREINCLUDE_INTEGER_H__
 // Built-in integer functions
+
+inline long clamp(long, long, long);
+
+// for int
+//inline uint abs(int x)	{	return x >= 0 ? x : -x;	}
+inline uint abs_diff(int x, int y)	{	return x > y ? x - y : y - x;	}
+inline int add_sat(int x, int y)
+{	return int(clamp(long(x) + long(y), -0x80000000, 0x7FFFFFFF));	}
+
+inline int hadd(int x, int y)
+{	return int(long(x) + long(y) >> 1);	}
+
+inline int rhadd(int x, int y)
+{	return int(long(x) + long(y) + 1 >> 1);	}
+
+inline int max(int, int);
+inline int min(int, int);
+inline int clamp(int x, int minval, int maxval)	{	return min(max(x, minval), maxval);	}
+inline int clz(int x)
+{
+	const uint y = (uint)x;
+	int c = ((y >> 16) == 0) << 4;
+	c += ((y >> (24 - c)) == 0) << 3;
+	c += ((y >> (28 - c)) == 0) << 2;
+	c += ((y >> (30 - c)) == 0) << 1;
+	c += ((y >> (31 - c)) == 0);
+	return c;
+}
+inline int mul_hi(int, int);
+inline int mad_hi(int a, int b, int c)	{	return mul_hi(a, b) + c;	}
+inline int mad_sat(int a, int b, int c)
+{	return int(clamp(long(a) * long(b) + long(c), (long)-0x80000000, (long)0x7FFFFFFF));	}
+
+inline int max(int x, int y)	{	return x > y ? x : y;	}
+inline int min(int x, int y)	{	return x < y ? x : y;	}
+inline int mul_hi(int x, int y)	{	return int(long(x) * long(y) >> 32);	}
+inline int rotate(int v, int i)	{	return (v << i) | (v >> (32 - i));	}
+
+inline int sub_sat(int x, int y)
+{	return int(clamp(long(x) - long(y), (long)-0x80000000, (long)0x7FFFFFFF));	}
+
+// for long
+inline long max(long, long);
+inline long min(long, long);
+inline long clamp(long x, long minval, long maxval)	{	return min(max(x, minval), maxval);	}
+
+inline long max(long x, long y)	{	return x > y ? x : y;	}
+inline long min(long x, long y)	{	return x < y ? x : y;	}
+
+// upsample functions
+inline short upsample(char hi, uchar lo)	{	return ((short)hi << 8) | lo;	}
+inline ushort upsample(uchar hi, uchar lo)	{	return ((ushort)hi << 8) | lo;	}
+inline int upsample(short hi, ushort lo)	{	return ((int)hi << 16) | lo;	}
+inline uint upsample(ushort hi, ushort lo)	{	return ((uint)hi << 16) | lo;	}
+inline long upsample(int hi, uint lo)	{	return ((long)hi << 32) | lo;	}
+inline ulong upsample(uint hi, uint lo)	{	return ((ulong)hi << 32) | lo;	}
+
+// fast integer built-in functions
+inline int mad24(int x, int y, int z)	{	return x * y + z;	}
+inline int mul24(int x, int y)	{	return x * y;	}
+
 #endif
