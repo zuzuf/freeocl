1199d215cb28621f37edd805d1c0993ded44e5be
zuzuf <zuzuf@ebb5cd76-4019-73bf-d3cb-c11e5ea2855e>
2011-04-30 21:59:42 +0000

    * implemented shuffle operations as well as vec_step (using typeof to support both types and expressions as argument)

--- include/FreeOCL/vmisc.h
+++ include/FreeOCL/vmisc.h
@@ -18,4 +18,46 @@
 #ifndef __FREEOCL_OPENCL_C_PREINCLUDE_VMISC_H__
 #define __FREEOCL_OPENCL_C_PREINCLUDE_VMISC_H__
 // Built-in miscellaneous vector functions
+// implement vec_step as a macro using templates and typeof
+template<class T> struct vec_step { enum { value = 1 }; };
+template<class T> struct vec_step<__vector_type<T, 2> > { enum { value = 2 }; };
+template<class T> struct vec_step<__vector_type<T, 3> > { enum { value = 4 }; };
+template<class T> struct vec_step<__vector_type<T, 4> > { enum { value = 4 }; };
+template<class T> struct vec_step<__vector_type<T, 8> > { enum { value = 8 }; };
+template<class T> struct vec_step<__vector_type<T, 16> > { enum { value = 16 }; };
+
+#define vec_step(X)	(vec_step<typeof(X)>::value)
+
+template<int N> struct __mask;
+template<> struct __mask<2> { enum { value = 0x1U }; };
+template<> struct __mask<3> { enum { value = 0x3U }; };
+template<> struct __mask<4> { enum { value = 0x3U }; };
+template<> struct __mask<8> { enum { value = 0x7U }; };
+template<> struct __mask<16> { enum { value = 0xFU }; };
+
+// shuffle
+template<typename gentypem, typename ugentypen>
+inline typename __vector_type<typename __vector<gentypem>::basic_type, __vector<ugentypen>::components>::type
+		shuffle(const gentypem &x, const ugentypen &mask)
+{
+	typename __vector_type<typename __vector<gentypem>::basic_type, __vector<ugentypen>::components>::type ret;
+	const size_t MASK = __mask<__vector<gentypem>::components>::value;
+	for(size_t i = 0 ; i < __vector<ugentypen>::components ; ++i)
+		ret.v[i] = x.v[mask.v[i] & MASK];
+	return ret;
+}
+
+// shuffle2
+template<typename gentypem, typename ugentypen>
+inline typename __vector_type<typename __vector<gentypem>::basic_type, __vector<ugentypen>::components>::type
+		shuffle2(const gentypem &x, const gentypem &y, const ugentypen &mask)
+{
+	typename __vector_type<typename __vector<gentypem>::basic_type, __vector<ugentypen>::components>::type ret;
+	const size_t MASK = __mask<__vector<gentypem>::components>::value;
+	const size_t MASK2 = MASK + 1U;
+	for(size_t i = 0 ; i < __vector<ugentypen>::components ; ++i)
+		ret.v[i] = (mask.v[i] & MASK2) ? y.v[mask.v[i] & MASK] : x.v[mask.v[i] & MASK];
+	return ret;
+}
+
 #endif
