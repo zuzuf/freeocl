7b3c598a0ef106454826fd16594ae309642b704b
nicknickolaev <nicknickolaev@gmail.com>
2011-09-20 00:04:00 +0000

    forgotten files from the previous commit:
    * Make C++0x usage optional. By default it's ON.
    * Depending on the C++0x usage flag use unorderd_map/map and unordered_set/set.
    Under FreeOCL::map and FreeOCL::set one can reimplement more efficient structures later

---
+++ src/utils/set.h
@@ -0,0 +1,41 @@
+/*
+	FreeOCL - a free OpenCL implementation for CPU
+	Copyright (C) 2011  Nikolay Nikolaev
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+#ifndef __FREEOCL_UTILS_SET_H__
+#define __FREEOCL_UTILS_SET_H__
+
+#ifdef FREEOCL_USE_CXX0X
+#include <unordered_set>
+#else
+#include <set>
+#endif
+
+namespace FreeOCL {
+
+template<typename _Key>
+class set: public
+#ifdef FREEOCL_USE_CXX0X
+		std::unordered_set<_Key>
+#else
+		std::set<_Key>
+#endif
+{
+};
+
+}
+
+#endif /* __FREEOCL_UTILS_SET_H__ */
