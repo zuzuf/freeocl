bbc1b2b85feff1c4df5a95f0a6b72e87e25492d5
zuzuf86 <zuzuf86@gmail.com>
2011-10-30 15:55:12 +0000

    * added full kernel support for image3d_t read/write functions

--- include/FreeOCL/imgreadwrite.h
+++ include/FreeOCL/imgreadwrite.h
@@ -91,8 +91,7 @@ inline float __quarter(float v)	{	return v * 0.25f;	}
 inline int __quarter(int v)	{	return v >> 2;	}
 inline uint __quarter(uint v)	{	return v >> 2;	}
 
-template<class I>
-inline int2 __address_mode(const I &image, sampler_t sampler, const int2 &coord)
+inline int2 __address_mode(const image2d_t &image, sampler_t sampler, const int2 &coord)
 {
 	int2 __coord;
 	switch(sampler & 0xFF)
@@ -112,6 +111,30 @@ inline int2 __address_mode(const I &image, sampler_t sampler, const int2 &coord)
 	case CLK_ADDRESS_MIRRORED_REPEAT:
 		__coord = coord % int2::make(image.width << 1, image.height << 1);
 		__coord = abs(__coord - int2::make(image.width, image.height));
+		break;
+	}
+	return __coord;
+}
+inline int4 __address_mode(const image3d_t &image, sampler_t sampler, const int4 &coord)
+{
+	int4 __coord;
+	switch(sampler & 0xFF)
+	{
+	case CLK_ADDRESS_NONE:
+		__coord = coord;
+		break;
+	case CLK_ADDRESS_CLAMP_TO_EDGE:
+		__coord = clamp(coord, int4::make(0), int4::make(image.width - 1, image.height - 1, image.depth - 1, 0));
+		break;
+	case CLK_ADDRESS_CLAMP:
+		__coord = clamp(coord, int4::make(-1), int4::make(image.width, image.height, image.depth, 0));
+		break;
+	case CLK_ADDRESS_REPEAT:
+		__coord = coord % int4::make(image.width, image.height, image.depth, 1);
+		break;
+	case CLK_ADDRESS_MIRRORED_REPEAT:
+		__coord = coord % int4::make(image.width << 1, image.height << 1, image.depth << 1, 1);
+		__coord = abs(__coord - int4::make(image.width, image.height, image.depth, 0));
 		break;
 	}
 	return __coord;
@@ -539,18 +562,327 @@ inline void write_imageui(image2d_t &image, const int2 &coord, const uint4 &colo
 	}
 }
 
-inline float4 read_imagef(const image3d_t &image, sampler_t sampler, const int4 &coord)	{}
-inline float4 read_imagef(const image3d_t &image, sampler_t sampler, const float4 &coord)	{}
+inline float4 read_imagef(const image3d_t &image, sampler_t sampler, const int4 &coord)
+{
+	const int4 __coord = __address_mode(image, sampler, coord);
 
-inline int4 read_imagei(const image3d_t &image, sampler_t sampler, const int4 &coord)	{}
-inline int4 read_imagei(const image3d_t &image, sampler_t sampler, const float4 &coord)	{}
+	const char * const ptr = (const char*)image.data
+							 + __coord.get<0>() * image.element_size
+							 + __coord.get<1>() * image.row_pitch
+							 + __coord.get<2>() * image.slice_pitch;
+	size_t nb_chan;
+	switch(image.channel_order)
+	{
+	case CLK_R:			nb_chan = 1;	break;
+	case CLK_A:			nb_chan = 1;	break;
+	case CLK_INTENSITY:	nb_chan = 1;	break;
+	case CLK_LUMINANCE:	nb_chan = 1;	break;
+	case CLK_Rx:		nb_chan = 1;	break;
 
-inline uint4 read_imageui(const image3d_t &image, sampler_t sampler, const int4 &coord)	{}
-inline uint4 read_imageui(const image3d_t &image, sampler_t sampler, const float4 &coord)	{}
+	case CLK_RG:		nb_chan = 2;	break;
+	case CLK_RA:		nb_chan = 2;	break;
+	case CLK_RGx:		nb_chan = 2;	break;
 
-inline void write_imagef(image3d_t &image, const int4 &coord, const float4 &color)	{}
-inline void write_imagei(image3d_t &image, const int4 &coord, const int4 &color)	{}
-inline void write_imageui(image3d_t &image, const int4 &coord, const uint4 &color)	{}
+	case CLK_RGB:		nb_chan = 3;	break;
+	case CLK_RGBx:		nb_chan = 3;	break;
+
+	case CLK_RGBA:		nb_chan = 4;	break;
+	case CLK_BGRA:		nb_chan = 4;	break;
+	case CLK_ARGB:		nb_chan = 4;	break;
+	}
+
+	float4 v;
+	switch(image.channel_data_type)
+	{
+	case CLK_UNORM_INT8:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			v.v[i] = ((const uchar*)ptr)[i] * (1.0f / 255.0f);
+		break;
+	case CLK_UNORM_INT16:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			v.v[i] = ((const ushort*)ptr)[i] * (1.0f / 65535.0f);
+		break;
+	case CLK_UNORM_SHORT_555:
+		{
+			const uint s = *(const ushort*)ptr;
+			v.get<0>() = ((s >> 10) & 31U) * (1.0f / 31.0f);
+			v.get<1>() = ((s >> 5) & 31U) * (1.0f / 31.0f);
+			v.get<2>() = (s & 31U) * (1.0f / 31.0f);
+		}
+		break;
+	case CLK_UNORM_SHORT_565:
+		{
+			const uint s = *(const ushort*)ptr;
+			v.get<0>() = ((s >> 11) & 31U) * (1.0f / 31.0f);
+			v.get<1>() = ((s >> 5) & 63U) * (1.0f / 63.0f);
+			v.get<2>() = (s & 31U) * (1.0f / 31.0f);
+		}
+		break;
+	case CLK_UNORM_INT_101010:
+		{
+			const uint s = *(const uint*)ptr;
+			v.get<0>() = ((s >> 20) & 1023U) * (1.0f / 1023.0f);
+			v.get<1>() = ((s >> 10) & 1023U) * (1.0f / 1023.0f);
+			v.get<2>() = (s & 1023U) * (1.0f / 1023.0f);
+		}
+		break;
+	case CLK_SNORM_INT8:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			v.v[i] = ((const char*)ptr)[i] * (1.0f / 127.0f);
+		break;
+	case CLK_SNORM_INT16:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			v.v[i] = ((const short*)ptr)[i] * (1.0f / 32767.0f);
+		break;
+	case CLK_HALF_FLOAT:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			v.v[i] = ((const half*)ptr)[i];
+		break;
+	case CLK_FLOAT:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			v.v[i] = ((const float*)ptr)[i];
+		break;
+	}
+
+	return __map_channels_for_reading(image, v);
+}
+inline float4 read_imagef(const image3d_t &image, sampler_t sampler, const float4 &coord)
+{
+	float4 rcoord = coord;
+	if (sampler & CLK_NORMALIZED_COORDS_TRUE)
+		rcoord *= float4::make(image.width, image.height, image.depth, 0.0f);
+	if (!(sampler & CLK_FILTER_LINEAR))
+		return read_imagef(image, sampler, convert_int4(rcoord));
+	const int4 c0 = convert_int4(rcoord - float4::make(0.5f, 0.5f, 0.5f, 0.f));
+	const float4 dp = rcoord - convert_float4(c0);
+	float4 v0 = read_imagef(image, sampler, c0);
+	float4 v1 = read_imagef(image, sampler, c0 + int4::make(1,0,0,0));
+	v0 += dp.get<0>() * (v1 - v0);
+	v1 = read_imagef(image, sampler, c0 + int4::make(0,1,0,0));
+	float4 v2 = read_imagef(image, sampler, c0 + int4::make(1,1,0,0));
+	v1 += dp.get<0>() * (v2 - v1);
+	v0 += dp.get<1>() * (v1 - v0);
+
+	v1 = read_imagef(image, sampler, c0 + int4::make(0,0,1,0));
+	v2 = read_imagef(image, sampler, c0 + int4::make(1,0,1,0));
+	v1 += dp.get<0>() * (v2 - v1);
+	v2 = read_imagef(image, sampler, c0 + int4::make(0,1,1,0));
+	const float4 v3 = read_imagef(image, sampler, c0 + int4::make(1,1,1,0));
+	v2 += dp.get<0>() * (v3 - v2);
+	v1 += dp.get<1>() * (v2 - v1);
+
+	return v0 + dp.get<2>() * (v1 - v0);
+}
+
+inline int4 read_imagei(const image3d_t &image, sampler_t sampler, const int4 &coord)
+{
+	const int4 __coord = __address_mode(image, sampler, coord);
+
+	const char * const ptr = (const char*)image.data
+							 + __coord.get<0>() * image.element_size
+							 + __coord.get<1>() * image.row_pitch
+							 + __coord.get<2>() * image.slice_pitch;
+	size_t nb_chan;
+	switch(image.channel_order)
+	{
+	case CLK_R:			nb_chan = 1;	break;
+	case CLK_A:			nb_chan = 1;	break;
+	case CLK_INTENSITY:	nb_chan = 1;	break;
+	case CLK_LUMINANCE:	nb_chan = 1;	break;
+	case CLK_Rx:		nb_chan = 1;	break;
+
+	case CLK_RG:		nb_chan = 2;	break;
+	case CLK_RA:		nb_chan = 2;	break;
+	case CLK_RGx:		nb_chan = 2;	break;
+
+	case CLK_RGB:		nb_chan = 3;	break;
+	case CLK_RGBx:		nb_chan = 3;	break;
+
+	case CLK_RGBA:		nb_chan = 4;	break;
+	case CLK_BGRA:		nb_chan = 4;	break;
+	case CLK_ARGB:		nb_chan = 4;	break;
+	}
+
+	int4 v;
+	switch(image.channel_data_type)
+	{
+	case CLK_SIGNED_INT8:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			v.v[i] = ((const char*)ptr)[i];
+		break;
+	case CLK_SIGNED_INT16:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			v.v[i] = ((const short*)ptr)[i];
+		break;
+	case CLK_SIGNED_INT32:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			v.v[i] = ((const int*)ptr)[i];
+		break;
+	}
+
+	return __map_channels_for_reading(image, v);
+}
+inline int4 read_imagei(const image3d_t &image, sampler_t sampler, const float4 &coord)
+{
+	if (sampler & CLK_NORMALIZED_COORDS_TRUE)
+		return read_imagei(image, sampler, convert_int4(coord * float4::make(image.width, image.height, image.depth, 0)));
+	return read_imagei(image, sampler, convert_int4(coord));
+}
+
+inline uint4 read_imageui(const image3d_t &image, sampler_t sampler, const int4 &coord)
+{
+	const int4 __coord = __address_mode(image, sampler, coord);
+
+	const char * const ptr = (const char*)image.data
+							 + __coord.get<0>() * image.element_size
+							 + __coord.get<1>() * image.row_pitch
+							 + __coord.get<2>() * image.slice_pitch;
+	size_t nb_chan;
+	switch(image.channel_order)
+	{
+	case CLK_R:			nb_chan = 1;	break;
+	case CLK_A:			nb_chan = 1;	break;
+	case CLK_INTENSITY:	nb_chan = 1;	break;
+	case CLK_LUMINANCE:	nb_chan = 1;	break;
+	case CLK_Rx:		nb_chan = 1;	break;
+
+	case CLK_RG:		nb_chan = 2;	break;
+	case CLK_RA:		nb_chan = 2;	break;
+	case CLK_RGx:		nb_chan = 2;	break;
+
+	case CLK_RGB:		nb_chan = 3;	break;
+	case CLK_RGBx:		nb_chan = 3;	break;
+
+	case CLK_RGBA:		nb_chan = 4;	break;
+	case CLK_BGRA:		nb_chan = 4;	break;
+	case CLK_ARGB:		nb_chan = 4;	break;
+	}
+
+	uint4 v;
+	switch(image.channel_data_type)
+	{
+	case CLK_UNSIGNED_INT8:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			v.v[i] = ((const uchar*)ptr)[i];
+		break;
+	case CLK_UNSIGNED_INT16:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			v.v[i] = ((const ushort*)ptr)[i];
+		break;
+	case CLK_UNSIGNED_INT32:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			v.v[i] = ((const uint*)ptr)[i];
+		break;
+	}
+
+	return __map_channels_for_reading(image, v);
+}
+inline uint4 read_imageui(const image3d_t &image, sampler_t sampler, const float4 &coord)
+{
+	if (sampler & CLK_NORMALIZED_COORDS_TRUE)
+		return read_imageui(image, sampler, convert_int4(coord * float4::make(image.width, image.height, image.depth, 0)));
+	return read_imageui(image, sampler, convert_int4(coord));
+}
+
+inline void write_imagef(image3d_t &image, const int4 &coord, const float4 &color)
+{
+	size_t nb_chan;
+	const float4 v = __map_channels_for_writing(image, color, nb_chan);
+	char *const ptr = (char*)image.data
+					  + image.row_pitch * coord.get<1>()
+					  + coord.get<0>() * image.element_size
+					  + coord.get<2>() * image.slice_pitch;
+	switch(image.channel_data_type)
+	{
+	case CLK_SNORM_INT8:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			((char*)ptr)[i] = clamp(v.v[i] * 127.0f, -127.0f, 127.0f);
+		break;
+	case CLK_SNORM_INT16:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			((short*)ptr)[i] = clamp(v.v[i] * 32767.0f, -32767.0f, 32767.0f);
+		break;
+	case CLK_UNORM_INT8:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			((uchar*)ptr)[i] = clamp(v.v[i] * 255.0f, 0.0f, 255.0f);
+		break;
+	case CLK_UNORM_INT16:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			((ushort*)ptr)[i] = clamp(v.v[i] * 65535.0f, 0.0f, 65535.0f);
+		break;
+	case CLK_UNORM_SHORT_565:
+		*((ushort*)ptr) = (((uint)clamp(v.get<0>() * 31.0f, 0.0f, 31.0f)) << 11)
+						| (((uint)clamp(v.get<1>() * 63.0f, 0.0f, 63.0f)) << 5)
+						| ((uint)clamp(v.get<2>() * 31.0f, 0.0f, 31.0f));
+		break;
+	case CLK_UNORM_SHORT_555:
+		*((ushort*)ptr) = (((uint)clamp(v.get<0>() * 31.0f, 0.0f, 31.0f)) << 10)
+						| (((uint)clamp(v.get<1>() * 31.0f, 0.0f, 31.0f)) << 5)
+						| ((uint)clamp(v.get<2>() * 31.0f, 0.0f, 31.0f));
+		break;
+	case CLK_UNORM_INT_101010:
+		*((uint*)ptr) = (((uint)clamp(v.get<0>() * 1023.0f, 0.0f, 1023.0f)) << 20)
+						| (((uint)clamp(v.get<1>() * 1023.0f, 0.0f, 1023.0f)) << 10)
+						| ((uint)clamp(v.get<2>() * 1023.0f, 0.0f, 1023.0f));
+		break;
+	case CLK_HALF_FLOAT:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			((half*)ptr)[i] = half::from_float(v.v[i]);
+		break;
+	case CLK_FLOAT:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			((float*)ptr)[i] = v.v[i];
+		break;
+	}
+}
+inline void write_imagei(image3d_t &image, const int4 &coord, const int4 &color)
+{
+	size_t nb_chan;
+	const int4 v = __map_channels_for_writing(image, color, nb_chan);
+	char *const ptr = (char*)image.data
+					  + image.row_pitch * coord.get<1>()
+					  + coord.get<0>() * image.element_size
+					  + coord.get<2>() * image.slice_pitch;
+	switch(image.channel_data_type)
+	{
+	case CLK_SIGNED_INT8:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			((char*)ptr)[i] = clamp(v.v[i], -128, 127);
+		break;
+	case CLK_SIGNED_INT16:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			((short*)ptr)[i] = clamp(v.v[i], -32768, 32767);
+		break;
+	case CLK_SIGNED_INT32:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			((int*)ptr)[i] = v.v[i];
+		break;
+	}
+}
+inline void write_imageui(image3d_t &image, const int4 &coord, const uint4 &color)
+{
+	size_t nb_chan;
+	const uint4 v = __map_channels_for_writing(image, color, nb_chan);
+	char *const ptr = (char*)image.data
+					  + image.row_pitch * coord.get<1>()
+					  + coord.get<0>() * image.element_size
+					  + coord.get<2>() * image.slice_pitch;
+	switch(image.channel_data_type)
+	{
+	case CLK_UNSIGNED_INT8:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			((uchar*)ptr)[i] = clamp(v.v[i], 0, 255);
+		break;
+	case CLK_UNSIGNED_INT16:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			((ushort*)ptr)[i] = clamp(v.v[i], 0, 65535);
+		break;
+	case CLK_SIGNED_INT32:
+		for(size_t i = 0 ; i < nb_chan ; ++i)
+			((uint*)ptr)[i] = v.v[i];
+		break;
+	}
+}
 
 inline int get_image_width(const image2d_t &image)	{	return image.width;	}
 inline int get_image_width(const image3d_t &image)	{	return image.width;	}
