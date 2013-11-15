ae65bb7ea3e2a2791a0274848958065e050da78d
zuzuf86 <zuzuf86@gmail.com>
2013-11-15 22:49:43 +0000

    * performance improvement to FreeOCL::to_string function for size_t and int types

--- src/utils/string.h
+++ src/utils/string.h
@@ -20,6 +20,7 @@
 
 #include <string>
 #include <sstream>
+#include <type_traits>
 
 namespace FreeOCL
 {
@@ -29,6 +30,101 @@ namespace FreeOCL
 		std::stringstream tmp;
 		tmp << v;
 		return tmp.str();
+	}
+
+	template<int N>	struct exp10	{	enum { value = exp10<N - 1>::value * size_t(10) };	};
+	template<>
+	struct exp10<0>	{	enum { value = 1 };	};
+
+	inline size_t log10_binary_search(size_t v)
+	{
+		if (v >= exp10<8>::value)
+		{
+			if (v >= exp10<12>::value)
+			{
+				if (v >= exp10<16>::value)
+				{
+					if (v >= exp10<18>::value)
+					{
+						if (v >= exp10<19>::value)
+							return 19;
+						return 18;
+					}
+					if (v >= exp10<17>::value)
+						return 17;
+					return 16;
+				}
+				if (v >= exp10<14>::value)
+				{
+					if (v >= exp10<15>::value)
+						return 15;
+					return 16;
+				}
+				if (v >= exp10<13>::value)
+					return 13;
+				return 12;
+			}
+			if (v >= exp10<10>::value)
+			{
+				if (v >= exp10<11>::value)
+					return 11;
+				return 10;
+			}
+			if (v >= exp10<9>::value)
+				return 9;
+			return 8;
+		}
+		if (v >= exp10<4>::value)
+		{
+			if (v >= exp10<6>::value)
+			{
+				if (v >= exp10<7>::value)
+					return 7;
+				return 6;
+			}
+			if (v >= exp10<5>::value)
+				return 5;
+			return 4;
+		}
+		if (v >= exp10<2>::value)
+		{
+			if (v >= exp10<3>::value)
+				return 3;
+			return 2;
+		}
+		if (v >= exp10<1>::value)
+			return 1;
+		return 0;
+	}
+
+	inline std::string to_string(const size_t &v)
+	{
+		if (v == 0)
+			return std::string(1, '0');
+		const size_t log10_v = log10_binary_search(v) + 1;
+		char buf[log10_v];
+		size_t w = v;
+		for(size_t i = 0 ; i < log10_v ; ++i, w /= 10)
+			buf[log10_v - i - 1] = '0' + (w % 10);
+
+		return std::string(buf, log10_v);
+	}
+
+	inline std::string to_string(const int &v)
+	{
+		if (v == 0)
+			return std::string(1, '0');
+		const bool b_neg = v < 0;
+		size_t w = b_neg ? -v : v;
+		const size_t log10_v = log10_binary_search(w) + (b_neg ? 2 : 1);
+		char buf[log10_v];
+		const size_t end = b_neg ? log10_v - 1 : log10_v;
+		for(size_t i = 0 ; i < end ; ++i, w /= 10)
+			buf[log10_v - i - 1] = '0' + (w % 10);
+		if (b_neg)
+			buf[0] = '-';
+
+		return std::string(buf, log10_v);
 	}
 }
 
